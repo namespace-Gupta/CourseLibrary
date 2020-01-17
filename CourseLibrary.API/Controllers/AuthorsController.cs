9710d989f52135917008599fc66fc86cd7082e0a
Mohit Gupta <AQQ@economical.com>
2020-01-17 16:21:46 -0600

    initial commit

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CourseLibrary.API.Services;
+
+namespace CourseLibrary.API.Controllers
+{
+
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet("api/authors")]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
